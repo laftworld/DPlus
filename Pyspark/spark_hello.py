7a91719d702596fb3242ee698ac8a0821e3e3124
WON EUI HONG <woneuihong@DAJUNGui-MacBook-Air.local>
2018-02-25 12:10:36 +0900

    update seclab branch

--- Pyspark/spark_hello.py
+++
@@ -1,7 +0,0 @@
-txt = sc.textFile("/example/data/gutenberg/davinci.txt")
-txt.count()
-txt.first()
-
-filtered_txt = txt.filter(lambda txt: "Leonardo" in txt)
-filtered_txt.count()
-filtered_txt.collect()
